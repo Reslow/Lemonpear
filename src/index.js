8ce73e06cc1b5e36329b09deb788b681b53a508b
Fanny Reslow <fr222er@student.lnu.se>
2021-04-24 09:09:27 +0200

    rewrite styles

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
+import Splide from '@splidejs/splide';
+
 import './styles.scss'
 
+new Splide( '.splide' ).mount();
+
+var elms = document.getElementsByClassName( 'splide' );
+for ( var i = 0, len = elms.length; i < len; i++ ) {
+	new Splide( elms[ i ] ).mount();
+}
